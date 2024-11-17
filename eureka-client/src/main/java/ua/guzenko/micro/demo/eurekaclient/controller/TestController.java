b603221730bdb8e5a26a6139e768f097dcc1e327
Dmitro Guzenko <guzenko.dmitriy@gmail.com>
2024-11-17 12:52:49 +0200

    instance, load balancer

--- eureka-client/src/main/java/ua/guzenko/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ua/guzenko/micro/demo/eurekaclient/controller/TestController.java
@@ -1,12 +1,19 @@
 package ua.guzenko.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "it's good";
+        return id;
     }
 }
